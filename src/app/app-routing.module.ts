7a288f5af1b0868134dc31398a996a26dfffd4aa
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-04-04 12:00:07 +0700

    change navbar cursor and add my/orders route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { LoginComponent } from './login/login.component';
@@ -10,18 +11,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'products', component: ProductsComponent},
-  { path: 'shopping-cart', component: ShoppingCartComponent},
-  { path: 'check-out', component: CheckOutComponent},
-  { path: 'order-success', component: OrderSuccessComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'admin/products', component: AdminProductsComponent},
-  { path: 'admin/orders', component: AdminOrdersComponent},
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'check-out', component: CheckOutComponent },
+  { path: 'order-success', component: OrderSuccessComponent },
+  { path: 'my/orders', component: MyOrdersComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin/products', component: AdminProductsComponent },
+  { path: 'admin/orders', component: AdminOrdersComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
