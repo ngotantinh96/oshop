a55cc5367fc87cb9f68e813749955f6dab20a2d0
ngotantinh96 <ngotantinh96@gmail.com>
2019-04-06 21:23:18 +0700

    populate dropdown data

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoryService } from "./../../category.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.css']
+  selector: "app-product-form",
+  templateUrl: "./product-form.component.html",
+  styleUrls: ["./product-form.component.css"]
 })
 export class ProductFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  categories$;
+  constructor(categoryService: CategoryService) {
+    this.categories$ = categoryService.getCategories();
   }
 
+  ngOnInit() {}
 }
