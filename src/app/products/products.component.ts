10542905ee36355f246e614099def867f15e2484
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-04-11 16:59:29 +0700

    filter by category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
 import { Component } from '@angular/core';
@@ -8,11 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: any[] = [];
+  filteredProducts: any[] = [];
   categories$;
+  category: string;
 
-  constructor(private productService: ProductService, private categoryService: CategoryService) {
-    this.products$ = this.productService.getAll();
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private categoryService: CategoryService
+  ) {
+    this.productService
+      .getAll()
+      .subscribe(products => (this.products = products));
+
     this.categories$ = this.categoryService.getAll();
+
+    this.route.queryParamMap.subscribe(x => {
+      this.category = x.get('category');
+
+      this.filteredProducts = this.category
+        ? this.products.filter(p => p.category === this.category)
+        : this.products;
+    });
   }
 }
