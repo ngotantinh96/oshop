8fca2d0dd9aed1fdd55ab4f3f5c163a81c31cc98
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-04-11 14:23:23 +0700

    display all products in ho,e page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,55 +1,55 @@
-import { ProductFormComponent } from "./admin/product-form/product-form.component";
-import { AdminAuthGuardService } from "./admin-auth-guard.service";
-import { AuthGuardService } from "./auth-guard.service";
-import { MyOrdersComponent } from "./my-orders/my-orders.component";
-import { AdminOrdersComponent } from "./admin/admin-orders/admin-orders.component";
-import { AdminProductsComponent } from "./admin/admin-products/admin-products.component";
-import { LoginComponent } from "./login/login.component";
-import { OrderSuccessComponent } from "./order-success/order-success.component";
-import { CheckOutComponent } from "./check-out/check-out.component";
-import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
-import { ProductsComponent } from "./products/products.component";
-import { HomeComponent } from "./home/home.component";
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { AdminAuthGuardService } from './admin-auth-guard.service';
+import { AuthGuardService } from './auth-guard.service';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { LoginComponent } from './login/login.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "products", component: ProductsComponent },
-  { path: "shopping-cart", component: ShoppingCartComponent },
-  { path: "login", component: LoginComponent },
+  { path: '', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'login', component: LoginComponent },
   {
-    path: "check-out",
+    path: 'check-out',
     component: CheckOutComponent,
     canActivate: [AuthGuardService]
   },
   {
-    path: "order-success",
+    path: 'order-success',
     component: OrderSuccessComponent,
     canActivate: [AuthGuardService]
   },
   {
-    path: "my/orders",
+    path: 'my/orders',
     component: MyOrdersComponent,
     canActivate: [AuthGuardService]
   },
   {
-    path: "admin/products",
+    path: 'admin/products',
     component: AdminProductsComponent,
     canActivate: [AuthGuardService, AdminAuthGuardService]
   },
   {
-    path: "admin/products/new",
+    path: 'admin/products/new',
     component: ProductFormComponent,
     canActivate: [AuthGuardService, AdminAuthGuardService]
   },
   {
-    path: "admin/products/:id",
+    path: 'admin/products/:id',
     component: ProductFormComponent,
     canActivate: [AuthGuardService, AdminAuthGuardService]
   },
   {
-    path: "admin/orders",
+    path: 'admin/orders',
     component: AdminOrdersComponent,
     canActivate: [AuthGuardService, AdminAuthGuardService]
   }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../product.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll();
   }
-
 }
