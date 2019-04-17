dcf7ee49794bd62c3d4dbe4f8f33fe39c58344fd
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-04-17 11:04:38 +0700

    add admin order and my order

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { OrderService } from '../order.service';
+import { AuthService } from '../auth.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.css']
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent {
+  orders$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService
+  ) {
+    this.orders$ = this.authService.user$.pipe(
+      switchMap(user => this.orderService.getOrdersByUser(user.uid))
+    );
   }
-
 }
